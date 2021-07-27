0072ef66a75ba4eb5d4243b7c4cb2147d594bcaf
minyul <ggomjae@gmail.com>
2021-07-27 22:23:46 +0900

    bindingResult 구현

--- src/main/kotlin/com/example/minyul/test/api/PutApi.kt
+++ src/main/kotlin/com/example/minyul/test/api/PutApi.kt
@@ -3,10 +3,14 @@ package com.example.minyul.test.api
 import com.example.minyul.test.dto.request.UserRequest
 import com.example.minyul.test.dto.response.Result
 import com.example.minyul.test.dto.response.UserResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.validation.FieldError
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.lang.StringBuilder
 import javax.validation.Valid
 
 @RestController
@@ -20,7 +24,21 @@ class PutApi {
 
     // Put은 내용이 없으면 생성하고 있으면 수정
     @PutMapping(path = ["/put-mapping/object"])
-    fun putMappingObject(@Valid @RequestBody request : UserRequest): UserRequest {
+    fun putMappingObject(@Valid @RequestBody request : UserRequest, bindingResult: BindingResult): UserRequest {
+
+
+        // 검증 후, 거기에 맞는 결과를 조작하고 싶으면 bindingResult를 쓰면 된다.
+//        if(bindingResult.hasErrors()) {
+//            // 500 Error
+//            val msg = StringBuilder()
+//            bindingResult.allErrors.forEach {
+//                val field = it as FieldError
+//                val message =it.defaultMessage
+//                msg.append(field.toString() + " :  " + message)
+//            }
+//            return ResponseEntity.badRequest().body(msg.toString())
+//        }
+
 
         val apply = UserResponse().apply {
             this.result = Result().apply {
