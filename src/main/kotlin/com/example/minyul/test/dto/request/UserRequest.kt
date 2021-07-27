33a090295ba11d9e0bf4b451eaa5c73db18bf078
minyul <ggomjae@gmail.com>
2021-07-27 22:30:45 +0900

    Validation이 일어날때 dataClass에 있는 메소드가 실행되게끔 하는 메소드 구현. @AssertTrue 를 사용해서! - yyyy-MM-dd HH:mm:ss 패턴

--- src/main/kotlin/com/example/minyul/test/dto/request/UserRequest.kt
+++ src/main/kotlin/com/example/minyul/test/dto/request/UserRequest.kt
@@ -3,6 +3,9 @@ package com.example.minyul.test.dto.request
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
+import java.lang.Exception
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import javax.validation.constraints.*
 
 
@@ -30,5 +33,21 @@ data class UserRequest (
     // phone_number 이럴 때 어떻게 바인딩을 해야할까? @JsonProperty로 가능
     @field:Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\{4}\$")
     @JsonProperty("phone_number")
-    var phoneNumber:String?= null
-)
+    var phoneNumber:String?= null,
+
+    var createdAt:String?= null // yyyy-MM-dd HH:mm:ss  ex ) 2028-10-02 13:00:00
+){
+
+
+
+    // 정상일때 true, 비정상 false
+    @AssertTrue // 검증하는 과정에서 실행할 수 있도록, Validation이 일어날때 이 메소드가 일어남
+    private fun inValidCreatedAt(): Boolean {
+        return try {
+            LocalDateTime.parse(this.createdAt, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+            true
+        } catch (e:Exception) {
+            false
+        }
+    }
+}
